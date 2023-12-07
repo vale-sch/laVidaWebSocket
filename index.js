7afd3630c58f2026932300887f47e5df526d732d
vale-sch <vali.vogt@live.de>
2023-12-07 11:49:37 +0100

    socket.io starting point

--- index.js
+++ index.js
@@ -14,18 +14,3 @@ io.on("connection", (socket) => {
 });
 
 http.listen(8080, () => console.log("listening on http://localhost:8080"));
-
-// Regular Websockets
-
-// const WebSocket = require('ws')
-// const server = new WebSocket.Server({ port: '8080' })
-
-// server.on('connection', socket => {
-
-//   socket.on('message', message => {
-
-//     socket.send(`Roger that! ${message}`);
-
-//   });
-
-// });
